987b1f940abe668a6960a3cff618b95a98057672
juanchos2018 <jcarlossenati@gmail.com>
2021-11-25 21:57:49 -0500

    primier

---
+++ ProyectoPrueba/FrmFormlario1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPrueba
+{
+    public partial class FrmFormlario1 : Form
+    {
+        public FrmFormlario1()
+        {
+            InitializeComponent();
+        }
+    }
+}
